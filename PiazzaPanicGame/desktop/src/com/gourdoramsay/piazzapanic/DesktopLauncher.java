1d932f3b6691e5aa0356cfd98d22b21e89531db2
laurenvictoriaa <117826360+laurenvictoriaa@users.noreply.github.com>
2023-01-16 12:38:33 +0000

    Initial Commit
    LibGDX gradle project generation

---
+++ PiazzaPanicGame/desktop/src/com/gourdoramsay/piazzapanic/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.gourdoramsay.piazzapanic;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gourdoramsay.piazzapanic.PiazzaPanic;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Piazza Panic");
+		new Lwjgl3Application(new PiazzaPanic(), config);
+	}
+}
